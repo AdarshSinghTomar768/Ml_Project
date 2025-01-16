cc64a050ef66d9097bef64d05b314c14785e134c
Adarsh Singh Tomar <adarshsinghtomar7909043383@gmail.com>
2025-01-16 15:16:02 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
   _,_,exc_tb = error_detail.exc_info() ## exc_tb will  tell the line number of the error and file name
